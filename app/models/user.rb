d77304c01e7f6037bd7fad39af6fde328ae35fcb
Nicklaus Pink <npink.coast@gmail.com>
2019-08-23 13:55:24 -0700

    Remove hours_worked code that was never implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,27 +15,12 @@ class User < ActiveRecord::Base
          
       }
       
-      @@hours_worked = {
-         'SS' => 18,
-         'NP' => 40,
-         'DP' => 18,
-         'KF' => 18,
-         'JT' => 40,
-         'GP' => 40,
-         'PP' => 40,
-         'SP' => 40
-      }
-      
       def User.users
          @@users
       end
       
       def get_full_name(initials)
          @@users[initials]
-      end
-      
-      def hours_worked(initials)
-         @@hours_worked[initials]
       end
       
    end
